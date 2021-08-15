4dd6241719440ae7969fabb695e2f8c6f1bfd308
Joao Galeao <jbgaleao@gmail.com>
2021-08-15 06:37:04 -0300

    Add project files.

---
+++ src/Arthesanatus2021.AppMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arthesanatus2021.AppMvc.MvcApplication" Language="C#" %>
---
+++ src/Arthesanatus2021.AppMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Arthesanatus2021.AppMvc.Startup))]
+namespace Arthesanatus2021.AppMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
