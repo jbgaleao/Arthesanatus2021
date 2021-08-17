2fdc0744dbe38fd922521c20e50af4f0ae26f519
CV7N <joao.galeao@spassu.com>
2021-08-17 01:39:56 -0300

    Configurando o DbContext

---
+++ src/Arthesanatus2021.Infra/Data/Context/Arthesanatus2021Context.cs
@@ -0,0 +1,18 @@
+﻿using Arthesanatus2021.Business.Models.Receitas;
+using Arthesanatus2021.Business.Models.Revistas;
+
+using System.Data.Entity;
+
+namespace Arthesanatus2021.Infra.Data.Context
+{
+    public class Arthesanatus2021Context : DbContext
+    {
+        public Arthesanatus2021Context() : base("ArthesConnection")
+        {
+
+        }
+
+        public DbSet<Revista> REVISTAS { get; set; }
+        public DbSet<Receita> RECEITAS { get; set; }
+    }
+}
---
+++ src/Arthesanatus2021.Infra/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Arthesanatus2021.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Arthesanatus2021.Infra.Data.Context.Arthesanatus2021Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Arthesanatus2021.Infra.Data.Context.Arthesanatus2021Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
