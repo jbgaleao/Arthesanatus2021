6c8e7164fec08cd92047b7100156ec71c1d2a429
CV7N <joao.galeao@spassu.com>
2021-08-20 22:49:50 -0300

    Instalando Fluent Validation

---
+++ src/Arthesanatus2021.Business/Models/Revistas/Validations/RevistaValidation.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace Arthesanatus2021.Business.Models.Revistas.Validations
+{
+    public class RevistaValidation : AbstractValidator<Revista>
+    {
+        public RevistaValidation()
+        {
+            RuleFor(r => r.NumeroEdicao)
+                .NotEmpty()
+                .WithMessage("Informe o {PropertyName}")
+                .NotEqual(0)
+                .InclusiveBetween(1, 999);
+
+            RuleFor(r => r.MesEdicao)
+                .NotEmpty()
+                .WithMessage("Informe o {PropertyName}");
+
+            RuleFor(r => r.AnoEdicao)
+                .NotEmpty()
+                .WithMessage("Informe o {PropertyName}");
+
+            RuleFor(r => r.Tema)
+                .NotEmpty()
+                .WithMessage("Informe o {PropertyName}")
+                .MaximumLength(150);
+
+            RuleFor(r => r.Foto)
+                .NotEmpty()
+                .WithMessage("Informe o Caminho da {PropertyName}")
+                .MaximumLength(500);
+        }
+    }
+}
