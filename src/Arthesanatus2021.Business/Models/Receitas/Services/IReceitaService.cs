8a66902dd09ce431307117c0b43b4963636640a6
CV7N <joao.galeao@spassu.com>
2021-08-21 22:45:13 -0300

    Implementar ReceitaRepository e sua Interface

---
+++ src/Arthesanatus2021.Business/Models/Receitas/Services/IReceitaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arthesanatus2021.Business.Models.Receitas.Services
+{
+    public interface IReceitaService :IDisposable
+    {
+        Task Adicionar(Receita receita);
+        Task Atualizar(Receita receita);
+        Task Remover(Guid id);
+    }
+}
---
+++ src/Arthesanatus2021.Business/Models/Receitas/Services/ReceitaService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Arthesanatus2021.Business.Core.Services;
+using Arthesanatus2021.Business.Models.Receitas.Validations;
+
+namespace Arthesanatus2021.Business.Models.Receitas.Services
+{
+    public class ReceitaService : BaseService, IReceitaService
+    {
+        private readonly IReceitaRepository _receitaRepository;
+
+        public ReceitaService(IReceitaRepository receitaRepository)
+        {
+            _receitaRepository = receitaRepository;
+        }
+
+        public async Task Adicionar(Receita receita)
+        {
+            if (!ExecutarValidacao(new ReceitaValidation(), receita))
+                return;
+
+            await _receitaRepository.Adicionar(receita);
+        }
+
+        public async Task Atualizar(Receita receita)
+        {
+            if (!ExecutarValidacao(new ReceitaValidation(), receita))
+                return;
+
+            await _receitaRepository.Atualizar(receita);
+        }
+
+        public async Task Remover(Guid id)
+        {
+            await _receitaRepository.Remover(id);
+        }
+         
+        public void Dispose()
+        {
+            _receitaRepository?.Dispose();
+        }
+    }
+}
