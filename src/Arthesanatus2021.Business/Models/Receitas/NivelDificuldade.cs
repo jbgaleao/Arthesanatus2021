254667d2ccb83200a6bcbf53542ea9250c7901a8
Joao Galeao <jbgaleao@gmail.com>
2021-10-15 01:29:56 -0300

    criacao partialView

--- src/Arthesanatus2021.Business/Models/Receitas/NivelDificuldade.cs
+++ src/Arthesanatus2021.Business/Models/Receitas/NivelDificuldade.cs
@@ -5,7 +5,7 @@ namespace Arthesanatus2021.Business.Models.Receitas
     public enum NivelDificuldade
     {
         [Description("AVANÇADO")]
-        Avacado = 1,
+        Avancado = 1,
         [Description("INTERMEDIÁRIO")]
         Intermediario = 2,
         [Description("BÁSICO")]
